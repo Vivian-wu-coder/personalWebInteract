1dd5a96d7863b6d13c2a393c18a9216c2f8064f5
Vivian Wu <weixian@uw.edu>
2020-10-21 00:38:43 -0700

    finish drag/drop

--- index.js
+++ index.js
@@ -8,16 +8,36 @@
 
     let profileButton = findElementById("profileButton");
     profileButton.addEventListener("click", profileButtonClick);
+
+    findElementById("quoteWords").addEventListener("dragstart", function(event) {dragQuoteWords(event)});
+    findElementById("quotePosition").addEventListener("drop", function(event) {dropOnQuotePosition(event)})
+    findElementById("quotePosition").addEventListener("dragover", function(event) {allowDropOnQuotePosition(event)})
+
   }
 
   function welcomeWord() {
-    alert("Welcome to Vivian's profile :)")
+    console.log("Welcome to Vivian's profile :)")
   }
 
   function profileButtonClick() {
     console.log("click on the button")
     findElementById("profileButton").innerHTML = "<a href=\"videochannel.html\">Video Channel</a>"
+  }
 
+  function dragQuoteWords(e) {
+    console.log("start dragging");
+    e.dataTransfer.setData("QuoteWords", e.target.id);
+  }
+
+  function dropOnQuotePosition(e) {
+    console.log("start dropping on");
+    e.preventDefault();
+    let quoteWords=e.dataTransfer.getData("QuoteWords");
+    e.target.parentNode.replaceChild(document.getElementById(quoteWords), findElementById("quotePosition"));
+  }
+
+  function allowDropOnQuotePosition(e) {
+    e.preventDefault();
   }
 
   function findElementById(idName) {
